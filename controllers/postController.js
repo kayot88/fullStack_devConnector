c0fa6b62523dea895e97eaa6d69a4fa5bd0a30f7
Kayot88 <kayot88@gmail.com>
2019-06-29 18:37:47 +0300

    post model&crud

--- controllers/postController.js
+++ controllers/postController.js
@@ -55,3 +55,25 @@ exports.getPostById = async (req, res) => {
     res.status(500).send('server error');
   }
 };
+exports.deletePost = async (req, res) => {
+  try {
+    const user = await User.findById({ _id: req.user.id });
+    const post = await Posts.findById(req.params.id);
+
+    if (post.user.toString() !== user.id) {
+      return res.status(401).json({ msg: 'You must be post owner' });
+    }
+
+    if (!post) {
+      return res.status(400).json({ msg: 'post not found' });
+    }
+    await post.remove();
+    res.json({ msg: 'post was delete' });
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'post not found' });
+    }
+    res.status(500).send('server error');
+  }
+};
--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const {
   addPost,
   getAllPosts,
-  getPostById
+  getPostById,
+  deletePost
 } = require('../controllers/postController');
 const { check, validationResult } = require('express-validator/check');
 const auth = require('../middleware/auth');
@@ -31,26 +32,5 @@ router.get('/', auth, getAllPosts);
 router.get('/:id', auth, getPostById);
 
 //delete post
-router.delete('/:id', auth, async (req, res) => {
-  try {
-    const user = await User.findById({_id:req.user.id});
-    const post = await Posts.findById(req.params.id);
-    
-    if (post.user.toString() !== user.id) {
-      return res.status(401).json({msg: 'You must be post owner'})
-    }
-
-    if (!post) {
-      return res.status(400).json({ msg: 'post not found' });
-    }
-    await post.remove()
-    res.json({ msg: 'post was delete' });
-  } catch (error) {
-    console.error(error.message);
-    if (error.kind === 'ObjectId') {
-      return res.status(400).json({ msg: 'post not found' });
-    }
-    res.status(500).send('server error');
-  }
-});
+router.delete('/:id', auth, deletePost);
 module.exports = router;
