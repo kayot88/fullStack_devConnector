42f6b5d890abd9b46d0418955eb376f98052d8f8
Kayot88 <kayot88@gmail.com>
2019-06-29 18:36:36 +0300

    posts crud

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,57 @@
-exports.postController = function (req, res) {
- res.send('post route')
+const { check, validationResult } = require('express-validator/check');
+const Posts = require('../models/Posts');
+const User = require('../models/User');
+const Profile = require('../models/Profile');
+
+exports.addPost = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    const user = await User.findById({ _id: req.user.id }).select('-password');
+    const post = new Posts({
+      text: req.body.text,
+      user: user.id,
+      avatar: user.avatar
+    });
+    await post.save();
+    res.status(200).json({ msg: 'The Post is created' });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+};
+
+exports.getAllPosts = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return res.status(401).json({ msg: 'You must authorize' });
+    }
+    const posts = await Posts.find().sort({ date: -1 });
+    if (!posts) {
+      return res.status(400).json({ msg: 'posts not found' });
+    }
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('server error');
+  }
+};
+
+exports.getPostById = async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (!post) {
+      return res.status(400).json({ msg: 'posts not found' });
+    }
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'posts not found' });
+    }
+    res.status(500).send('server error');
+  }
 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,56 @@
 const router = require('express').Router();
-const { postController } = require('../controllers/postController');
-//route GET api/users
-router.get('/', postController);
+const {
+  addPost,
+  getAllPosts,
+  getPostById
+} = require('../controllers/postController');
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../middleware/auth');
+const Posts = require('../models/Posts');
+const User = require('../models/User');
+const Profile = require('../models/Profile');
 
+//route POST create post
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  addPost
+);
+
+//get all posts
+router.get('/', auth, getAllPosts);
+
+//get post by id
+router.get('/:id', auth, getPostById);
+
+//delete post
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const user = await User.findById({_id:req.user.id});
+    const post = await Posts.findById(req.params.id);
+    
+    if (post.user.toString() !== user.id) {
+      return res.status(401).json({msg: 'You must be post owner'})
+    }
+
+    if (!post) {
+      return res.status(400).json({ msg: 'post not found' });
+    }
+    await post.remove()
+    res.json({ msg: 'post was delete' });
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'post not found' });
+    }
+    res.status(500).send('server error');
+  }
+});
 module.exports = router;
